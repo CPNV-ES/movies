6a65025025558f463fea4585bb765a7cb20d8c67
Jonathan Zaehringer <jonathan.zaehringer@cpnv.ch>
2015-12-08 16:51:15 +0100

    Fix InfoFilm and add english commentary

--- php/functions/lib_searchMovies.php
+++ php/functions/lib_searchMovies.php
@@ -1,11 +1,10 @@
 <?php
 
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getAllFilm
+	Return id and title of all film found in the db
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+	Return : Success = array of film, Echec = False	*/
 function getAllFilm($db){
 	$query  = 'SELECT movies.`idMovies`, movies.`Title` FROM movies';
 
@@ -25,12 +24,12 @@ function getAllFilm($db){
     return false;
 }
 
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getFilmByTitle
+	Return all movies when the string match on the title
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+		- search_string : string for matching on title
+	Return : Success = array of movies found, Echec = False	*/
 function getFilmByTitle($db, $search_string){
 	$query  = 'SELECT movies.`idMovies`, movies.`Title` FROM movies ';
     $query .= 'WHERE movies.`Title` LIKE ?';
@@ -55,12 +54,12 @@ function getFilmByTitle($db, $search_string){
     return false;
 }
 
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getGenres
+	Return all Genres of a movies
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+		- id : id of Movies in DB
+	Return : Success = array of Genres, Echec = False	*/
 function getGenres($db, $id){
 	$query  = 'SELECT genres.`Name` FROM movies ';
     $query .= 'INNER JOIN genres_movies ON genres_movies.`fkMovies` = movies.`idMovies` ';
@@ -85,12 +84,12 @@ function getGenres($db, $id){
     return false;
 }
 
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getCountries
+	return all countries of a movies
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+		- id : id of Movies in DB
+	Return : Success = array of Genres, Echec = False	*/
 function getCountries($db, $id){
 	$query  = 'SELECT countries.`Name` FROM movies ';
     $query .= 'INNER JOIN countries_movies ON countries_movies.`fkMovies` = movies.`idMovies` ';
@@ -115,12 +114,13 @@ function getCountries($db, $id){
     return false;
 }
 
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getPeople
+	return all people with precised role of a movies
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+		- id : id of Movies in DB
+		- type : role of people
+	Return : Success = array of People, Echec = False	*/
 function getPeople($db, $id, $type){
 	$query  = 'SELECT people.`FirstName`,people.`LastName` FROM movies ';
     $query .= 'INNER JOIN people_movies ON people_movies.`fkMovies` = movies.`idMovies` ';
@@ -148,13 +148,54 @@ function getPeople($db, $id, $type){
     return false;
 }
 
-
-/* //TODO UDPATE Function getSourceId
-	Recupére l'id en base de donnée de la source demandé
+/* Function getStudios
+	return all studios of a movie
 	Param :
-		- db : lien vers la base de donnée (PDO object)
-		- path : chemin source demandé
-	Return : En succes = l'id de la source, Echéc = False	*/
+		- db : connector PDO of the db
+		- id : id of Movies in DB
+	Return : Success = array of Studios, Echec = False	*/
+function getStudios($db, $id){
+	$query  = 'SELECT people.`FirstName`,people.`LastName` FROM movies ';
+    $query .= 'INNER JOIN studios_movies ON studios_movies.`fkMovies` = movies.`idMovies` ';
+	$query .= 'INNER JOIN studios ON studios.`idStudios` = studios_movies.`fkStudios` ';
+	$query .= 'WHERE movies.`idMovies` = ?';
+
+    $req = $db->prepare($query);
+    $req->bindParam(1, $id);
+
+    if(!$req->execute()){
+        $error = $req->errorCode();
+        $error = "Erreur est survenu lors de l'execution de la requête ('$error')";
+		//echo $error;
+        return false;
+    }
+
+    if($req->rowCount() >= 1){
+        $result = $req->fetchAll();
+        return $result;
+    }
+
+    return false;
+}
+
+
+/* Function getInfoFilm
+	return all movies with all information. It's possible to precised an attribute of the movie
+	Param :
+		- db : connector PDO of the db
+		- attr : array of attribute /!\Special syntax !!!  Look example /!\
+	Return : Success = array of Movies, Echec = False
+
+	Example of attributes
+	- field is the field in database
+	- value : is the value for the test ('%r%' or '1', ...)
+	- sign : is the sign for the comparation for permit compart string with special test % or _ ( '=' or 'LIKE', ...)
+	$attr = array(
+			'field' => array(
+				'value', 'sign'
+			)
+		);
+	*/
 function getInfoFilm($db, $attr = array()){
 	$query  = 'SELECT * FROM movies ';
 	$first = true;
@@ -189,8 +230,8 @@ function getInfoFilm($db, $attr = array()){
 		$result[$i]["director"] = getPeople($db, $id, DB_DIRECTOR_TYPE);
 		$result[$i]["actor"] = getPeople($db, $id, DB_ACTOR_TYPE);
 		$result[$i]["producer"] = getPeople($db, $id, DB_PRODUCER_TYPE);
+		$result[$i]["studios"] = getStudios($db, $id);
 	}
-	var_dump($result);
 
-    return false;
+	return $result;
 }
