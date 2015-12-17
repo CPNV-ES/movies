39f169fff8d95cff6e2fc9af2b8fd36cda391832
Jonathan Zaehringer <jonathan.zaehringer@cpnv.ch>
2015-12-17 11:36:56 +0100

    add tokens system for security and front-end information

--- php/script/scan_files.php
+++ php/script/scan_files.php
@@ -1,16 +1,45 @@
 <?php
 	require_once("../configs/project_root.php");
+	require_once(ROOT_PATH."php/functions/lib_db_connect.php");
+	require_once(ROOT_PATH."php/functions/lib_db_token.php");
 	require_once(ROOT_PATH."php/configs/configs.php");
 	require_once(ROOT_PATH."php/functions/lib_files.php");
 	require_once(ROOT_PATH."php/functions/lib_movies.php");
 
-	$paths = array();
-	$paths = array($_POST['path']);
-
-	$result = array();
-	if(getFilms($result, $paths) === false){
+	if(!isset($_POST['path']) || empty($_POST['path'])){
+		$error = "Path for scanning is empty !";
 		return false;
 	}
 
+	$paths = array($_POST['path']);
+
+	$db = connectDB();
+
+	if( ($paths_checking = getTokenChecking($db)) === false){
+		$error = "Internal server error";
+		return false;
+	}
+	elseif(!empty($paths_checking)){
+		foreach($paths_checking as $row){
+			$return = strpos($paths[0], $row["path"], 0);
+
+			if($return !== false){
+				$error = "This path is being processed !";
+				return false;
+			}
+		}
+	}
+
+	$id_token = setToken($db, $paths[0], "Getting all files in source");
+
+	$result = array();
+	if(getFilms($result, $path) === false){
+		removeToken($db, $id_token);
+		return false;
+	}
+
+	updateToken($db, $id_token, "Getting information for all movies find in the source");
 	recoverInfoMovies($result);
+
+	removeToken($db, $id_token);
 ?>
